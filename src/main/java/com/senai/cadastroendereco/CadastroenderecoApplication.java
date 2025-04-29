8eecd2bc71b72a4d974d14ecf12cedce1a683645
vedilsonprado <vedilsonprado@gmail.com>
2025-04-29 07:35:04 -0300

    Primeiro commit do Projeto Cadastro Endereço

---
+++ src/main/java/com/senai/cadastroendereco/CadastroenderecoApplication.java
@@ -0,0 +1,13 @@
+package com.senai.cadastroendereco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroenderecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroenderecoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senai/cadastroendereco/CadastroenderecoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.cadastroendereco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroenderecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
